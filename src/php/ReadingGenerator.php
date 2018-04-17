473491c07f60ced000a7061871849791a9a0565a
pietrek <pietrek@localhost.localdomain>
2018-04-17 20:46:36 +0200

    Linux support fixes - directory separators.

--- src/php/ReadingGenerator.php
+++ src/php/ReadingGenerator.php
@@ -1,5 +1,5 @@
 <?php
-  require_once(__DIR__."\HobbyGenerator.php");
+  require_once(__DIR__. DIRECTORY_SEPARATOR ."HobbyGenerator.php");
 
   class ReadingGenerator extends HobbyGenerator
   {
--- src/php/SemestersGenerator.php
+++ src/php/SemestersGenerator.php
@@ -1,5 +1,5 @@
 <?php
-   require_once(__DIR__."\BaseGenerator.php");
+   require_once(__DIR__. DIRECTORY_SEPARATOR ."BaseGenerator.php");
 
   class SemestersGenerator extends BaseGenerator
   {
