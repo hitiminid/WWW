51090d08e6d854cb23c57c2698a91daad747cb02
piotr.kawa <piotr.w.kawa@gmail.com>
2018-05-31 00:44:06 +0200

    LocalStorageUtility introduced.

---
+++ src/project_temp/js/localStorageUtility.js
@@ -0,0 +1,70 @@
+
+/**
+ * Przekształcenie obrazka z sieci w reprezentację Base64
+ * Base64 to metoda konwertowania ciągu bitów na ciąg wybranych 64 znaków ASCII
+ * @param {string} src : adres URL obrazka
+ * @param {function(string):undefined} callback : funkcja która zrobi coś z przekształconym
+ *    obrazkiem
+*/
+function toDataURL(src, callback) {
+    var img = new Image();
+    img.crossOrigin = "anonymous";
+    img.onload = function() {
+      var canvas = document.createElement('CANVAS');
+      var ctx = canvas.getContext('2d');
+      var dataURL;
+      canvas.height = this.naturalHeight;
+      canvas.width = this.naturalWidth;
+      ctx.drawImage(this, 0, 0);
+      dataURL = canvas.toDataURL();
+      callback(dataURL);
+    };
+    img.src = src;
+    if (img.complete || img.complete === undefined) {
+      //one-pixel gif
+      img.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
+      img.src = src;
+    }
+  }
+  
+  /**
+   * Class constructor 
+   * @param {string} imageId identyfikator obiektu img
+   * @param {string} localStorageKey klucz w lokalnej pamięci
+   * @param {string} imgSrc adres URL obrazka do załadowania
+   * @constructor
+   */
+  function ImageLoader(imageId, localStorageKey, imgSrc) {
+    this.imageId         = imageId;
+    this.localStorageKey = localStorageKey;
+    this.imgSrc          = imgSrc;
+  }
+  
+  ImageLoader.prototype.loadImage = function() {
+      var that = this;
+      var img = document.getElementById(this.imageId);
+      if (localStorage.getItem(this.localStorageKey) !== null) {
+        img.src = localStorage.getItem(this.localStorageKey);
+      } else {
+        toDataURL(
+          that.imgSrc,
+          function(dataUrl) {
+            localStorage.setItem(that.localStorageKey, dataUrl);
+            img.src=dataUrl;
+          }
+        )
+      }
+  };
+  
+  ImageLoader.prototype.clearLocalStorage = function() {
+      localStorage.removeItem(this.localStorageKey);
+  };
+
+//   var myImg = new ImageLoader("face-image", "faceImage", "../img/face.png");
+//   myImg.loadImage();
+
+//   localStorage.removeItem("galacticaImg");
+
+  function czysc() {
+    myImg.clearLocalStorage();
+  }
--- src/project_temp/php/content_generators/HomePageGenerator.php
+++ src/project_temp/php/content_generators/HomePageGenerator.php
@@ -12,7 +12,7 @@
       <div id='panorama'>
         <img src='$backgroundPath' alt='$altBackground'/>
         <div id='panorama-panel'>
-          <img src='$facePath' alt='$altFace'/>
+          <img id='face-image' src='$facePath' alt='$altFace'/>
           <h2>$name</h2>
         </div>
       </div>\n";
