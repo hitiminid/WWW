887865508654da789d210a0613cd17c5dfaa340a
piotr.kawa <piotr.w.kawa@gmail.com>
2018-06-02 09:25:36 +0200

    Creating comment appends it to DOM.

--- src/project_3/src/js/comments.js
+++ src/project_3/src/js/comments.js
@@ -17,11 +17,13 @@ validateData = () => {
 }
 
 appendCurrentlyCreatedComment = (author, avatar, date,text) => {
-    let commentStart_1 = "<div class='comment'></div><div class='comment-header'><div class='children'>";
-    let commentStart_2 = "<div class='image-panel'><img src='${avatar}'></div>";
-    let commentStart_3 = "<div class='comment-info'><h6 class='comment-author'>$author</h6><h6 class='comment-date'>${date}</h6></div>"
-    let commentBody = "<div class='comment-body'><p class='comment-text'>${ text }</p></div>";
-
+    let commentStart_1 = "<div class='comment'><div class='comment-header'><div class='children'>";
+    let commentStart_2 = `<div class='image-panel'><img src='${avatar}'></div>`;
+    let commentStart_3 = `<div class='comment-info'><h6 class='comment-author'>${author}</h6><h6 class='comment-date'>${date}</h6></div></div></div>`;
+    let commentBody    = `<div class='comment-body'><p class='comment-text'>${text}</p></div></div>`;
+    let comment = commentStart_1 + commentStart_2 + commentStart_3 + commentBody;
+    console.log(comment);
+    $("#comment-section-body").append(comment);
     // todo: append 
 }
 
@@ -40,13 +42,18 @@ function sendComment(event) {
     // let isDataValid = true;
 
     if (isDataValid) {
-    
+        let author = document.getElementById('comment-author-input-field').value;
+        let title  = document.getElementById('comment-author-input-field').value;
+        let text   = document.getElementById('comment-text-area').value;
+        let date = "";
+
         var myData = {
-            commentAuthor: $('#comment-author-input-field').val(),
-            commentTitle: $('#comment-author-input-field').val(),
-            commentText: $('#comment-text-area').val(),
+            commentAuthor: author,
+            commentTitle: title,
+            commentText: text,
             pageId: 1
         };
+        appendCurrentlyCreatedComment(author, "", date, text);
         // phpScriptURL = getProperPHPFilePath(event.data.pageId);
         phpScriptURL = "../../php/database_utilities/submit_comment.php";
 
