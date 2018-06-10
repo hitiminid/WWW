55cb69a5029135aa7c36a996881bdf6c27219181
piotr.kawa <piotr.w.kawa@gmail.com>
2018-06-10 22:27:41 +0200

    One task compiles both compressed and expanded version.

--- src/project_3/src/gulpfile.js
+++ src/project_3/src/gulpfile.js
@@ -1,26 +1,37 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp   = require('gulp');
+var sass   = require('gulp-sass');
+var rename = require('gulp-rename');
 
-
-var ouputStyleExpanded = {
+var outputStyleExpanded = {
     errLogToConsole: true,
     outputStyle: 'expanded',
     precision: 8
 };
 
-var ouputStyleCompressed = {
+var outputStyleCompressed = {
     errLogToConsole: true,
     outputStyle: 'compressed',
     precision: 8
 }; 
 
+// one task compiles both compressed and expanded version
 gulp.task('reload',function() {
-    gulp.watch('scss/**/*.scss',['styles']);
+    gulp.watch('scss/**/*.scss',['reloadFiles']);
 });
+
+gulp.task('reloadFiles', ['styles', 'compressed']);
 
 gulp.task('styles', function() {
     return gulp.src('scss/**/styles.scss')
-        .pipe(sass(ouputStyleExpanded))
+        .pipe(sass(outputStyleExpanded))
         .on('error', sass.logError)
+        .pipe(gulp.dest('./css/'))
+});     
+
+gulp.task('compressed', function() {
+    return gulp.src('scss/**/styles.scss')
+        .pipe(sass(outputStyleCompressed))
+        .on('error', sass.logError)
+        .pipe(rename('styles-min.css'))
         .pipe(gulp.dest('./css/'))
 });     
